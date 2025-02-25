e0bf8ef3f8da6f94d0d8cc7c7f0ba72bba2e3594
Banai Adrián Márton <banai.adrian.marton@students.jedlik.eu>
2025-02-25 21:19:11 +0100

    Admin felület kezdetleges kialakítása

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrl: './admin.component.css',
 })
 export class AdminComponent {
+  items: string[] = ['Elem 1', 'Elem 2', 'Elem 3'];
+  newItem: string = '';
 
+  addItem() {
+    if (this.newItem.trim()) {
+      this.items.push(this.newItem);
+      this.newItem = '';
+    }
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
