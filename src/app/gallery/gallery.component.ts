cd76609ec7ffa6e6ddb24d5fc1a492c20e8bd8dc
Banai Adrián Márton <banai.adrian.marton@students.jedlik.eu>
2025-04-01 20:50:38 +0200

    Galléria componens képeinek véglegesítése

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,20 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css',
 })
 export class GalleryComponent {
-  images: string[] = [
-    'minta.jpg',
-    'minta.jpg',
-    'minta.jpg',
-    'minta.jpg',
-    'minta.jpg',
-    'minta.jpg',
-    'minta.jpg',
-    'minta.jpg',
-  ];
+  images: string[] = Array.from(
+    { length: 18 },
+    (_, i) => `photos/pic${i + 1}.jpg`
+  );
+
+  selectedImage: string | null = null;
+
+  openModal(image: string) {
+    this.selectedImage = image;
+  }
+
+  closeModal() {
+    this.selectedImage = null;
+  }
 }
